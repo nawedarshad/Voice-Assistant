5ff844ea0f252f63daa931110929c07b14224739
nawedarshad <nawedarshad25@gmail.com>
2024-08-27 08:10:00 +0530

    Data File Added

--- main.py
+++ main.py
@@ -1,60 +1,64 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import data
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
 re_check = True
-data = {
-    "hello_list": [
-    "hi",
-    "hello",
-    "hey",
-    "greetings",
-    "howdy",
-    "hiya",
-    "what's up",
-    "sup",
-    "yo",
-    "good day",
-    "hey there",
-],
-    "bye_list": [
-    "bye",
-    "goodbye",
-    "see you",
-    "later",
-    "farewell",
-    "take care",
-    "see ya",
-    "bye-bye",
-    "good night",
-]
-
-}
-
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 def commandProcess(command):
-     for i in data["hello_list"]: 
+     
+    for i in data.data["hello_list"]: 
         if command.lower() == i:
             reply = "Hii, how can I help you?"
             print(reply)
             speak(reply)
             return 
-     for i in data["bye_list"]: 
+    
+    for i in data.data["websites"]: 
+        a = "open " + i
+        if command.lower() == a:
+            reply = f"Opening {i}"
+            print(reply)
+            speak(reply)
+            webbrowser.open(f"https://{i}.com")
+            return 
+
+
+    for i in data.data["open_browser_phrases"]: 
+        if command.lower() == i:
+            reply = "Tell me the Domain name"
+            print(reply)
+            speak(reply)
+            try:
+                with sr.Microphone() as source:
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+                    print(f"Opening URL: {command}")
+                    webbrowser.open(f"https://{command}")
+                    
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+            except sr.RequestError as e:
+                print("Error; {0}".format(e))
+            return
+        
+     
+    for i in data.data["bye_list"]: 
         if command.lower() == i:
             reply = "Goodbye!!"
             print(reply)
             speak(reply)
             global re_check
             re_check = False
+    
                 
      
-
 if __name__ == "__main__":
     speak("Initializing your personal Voice Assistant")
 
